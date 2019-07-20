847d96fa70366fc3a9ccc952871eb327189b7d59
jamiejamiebobamie <jmccrory@vt.edu>
2019-07-20 10:06:58 -0700

    midway through the first challenge

--- src/App.js
+++ src/App.js
@@ -3,18 +3,43 @@ import inventory, { categories } from './inventory'
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {chosenCategory: ""};
+    }
+
+
 
   render() {
+
+      function displayProducts(props){
+          const category = props.category
+          if (this.state.chosenCategory === category){
+              const results = inventory.filter(item => item.category === category);
+              const products =
+              (results.map((result) =>
+                  <li>{result.name} {result.category} {result.price}</li>)); // need to include array key here*                                                      // https://reactjs.org/docs/lists-and-keys.html
+              return (
+                  products
+          );
+          }
+      }
+
+    console.log({categories});
+    console.log(categories);
+    const listItems =
+    (categories.map((category) =>
+        <li><button>{category}</button></li>)); // need to include array key here*
+                                               // https://reactjs.org/docs/lists-and-keys.html
+    console.log(listItems)
     return (
       <div className="App">
         <h1>Show products here</h1>
-
+            <ul class="categories">
+            {listItems}
+            </ul>
         <ul>
-          {/* List product categories here */}
-        </ul>
-
-        <ul>
-          {/* Products listed here */}
+          <displayProducts category={this.state.category} />
         </ul>
 
       </div>
