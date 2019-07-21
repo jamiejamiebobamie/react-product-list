87954292fd2bb3a854927436335db81f1f82401c
jamiejamiebobamie <jmccrory@vt.edu>
2019-07-20 19:58:19 -0700

    added a changeCategory and displayProducts method

--- src/App.js
+++ src/App.js
@@ -2,36 +2,50 @@ import React, { Component } from 'react';
 import inventory, { categories } from './inventory'
 import './App.css';
 
+
+// class LoggingButton extends React.Component {
+//   // This syntax ensures `this` is bound within handleClick.
+//   // Warning: this is *experimental* syntax.
+//   handleClick = () => {
+//     this.props.onButtonClick(this.props.value);
+//   }
+//
+//   render() {
+//     return (
+//       <button onClick={this.handleClick}>
+//         {this.props.column}
+//       </button>
+//     );
+//   }
+// }
+
+
+
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {chosenCategory: ""};
     }
 
+    changeCategory(props) {
+        this.setState({chosenCategory: props.category});
+    }
 
+    displayProducts() {
+        const category = this.state.chosenCategory
+        const results = inventory.filter(item => item.category === category);
+        const products =
+        (results.map((result) =>
+            <li>{result.name} {result.category} {result.price}</li>)); // need to include array key here* //https://reactjs.org/docs/lists-and-keys.html
+        return products
+    }
 
   render() {
 
-      function displayProducts(props){
-          const category = props.category
-          if (this.state.chosenCategory === category){
-              const results = inventory.filter(item => item.category === category);
-              const products =
-              (results.map((result) =>
-                  <li>{result.name} {result.category} {result.price}</li>)); // need to include array key here*                                                      // https://reactjs.org/docs/lists-and-keys.html
-              return (
-                  products
-          );
-          }
-      }
-
-    console.log({categories});
-    console.log(categories);
     const listItems =
     (categories.map((category) =>
-        <li><button>{category}</button></li>)); // need to include array key here*
-                                               // https://reactjs.org/docs/lists-and-keys.html
-    console.log(listItems)
+        <li><button onClick={() => this.changeCategory({category})}>{category}</button></li>)); // need to include array key here*
+                                                                                                // https://reactjs.org/docs/lists-and-keys.html
     return (
       <div className="App">
         <h1>Show products here</h1>
@@ -39,7 +53,7 @@ class App extends Component {
             {listItems}
             </ul>
         <ul>
-          <displayProducts category={this.state.category} />
+            {this.displayProducts()}
         </ul>
 
       </div>
