382c8abc3f351793147042535c1b023b77299680
jamiejamiebobamie <jmccrory@vt.edu>
2019-08-27 21:44:14 -0700

    styled the output of the price

--- src/PriceBar.js
+++ src/PriceBar.js
@@ -5,6 +5,7 @@ import './PriceBar.css'
 function Pricebar(props){
     const category = props.currentCategory
     let price = 0;
+    let results
 
     function add(number1,number2) {
         return number1+number2
@@ -12,34 +13,30 @@ function Pricebar(props){
 
     let productPricesArray = []
     if (category === "All"){
-        const results = inventory;
-        for (let i = 0; i < results.length; i++){
-            productPricesArray.push(parseFloat(results[i].price))
-        }
-        price = productPricesArray.reduce(add)
-        price = productPricesArray.reduce(add)
-        price = price.toString()
-        price = price.split(".")
-        if (price.length > 1){
-            price = price[0] + "." + price[1].slice(0,2)
-        }
-
+        results = inventory;
     } else {
-        const results = inventory.filter(item => item.category === category);
-        for (let i = 0; i < results.length; i++){
-            productPricesArray.push(parseFloat(results[i].price))
+        results = inventory.filter(item => item.category === category);
+    }
+
+    for (let i = 0; i < results.length; i++){
+        productPricesArray.push(parseFloat(results[i].price))
+    }
+
+    price = productPricesArray.reduce(add)
+    price = price.toString()
+    price = price.split(".")
+    if (price.length > 1){
+        if (price[1].length < 2 ){
+            price[1] = price[1]+ '0'
         }
-        price = productPricesArray.reduce(add)
-        price = price.toString()
-        price = price.split(".")
-        if (price.length > 1){
-            price = price[0] + "." + price[1].slice(0,2)
-        }
+        price = price[0] + "." + price[1].slice(0,2)
+    } else {
+        price = price[0] + " dollars"
     }
 
     return (
         <div className="Pricebar">
-            <h1>the price for these products is {price} dollars</h1>
+            <h3>the price is {price}</h3>
         </div>
     )
 }
