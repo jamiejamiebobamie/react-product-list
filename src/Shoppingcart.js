ac273089a3413c1edad7a8bc5f770843feab2a7c
jamiejamiebobamie <jmccrory@vt.edu>
2019-08-30 10:09:07 -0700

    formatted shopping cart view

--- src/Shoppingcart.js
+++ src/Shoppingcart.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './Shoppingcart.css';
 import Pricebar from './Pricebar.js'
 import Cartproducts from './Cartproducts.js'
+import './Shoppingcart.css'
 
 
 function ShoppingCart(props){
+    const noItems = (
+        <div>
+        <img className="cartPicture" src="./imgs/cart_icon.png"alt=""/>
+        <h1 className="cartMessage">No items in cart</h1>
+        </div>
+    )
     return (
         <div className="ShoppingCart">
-        <img src="./imgs/cart_icon.png"alt=""/>
-        <Cartproducts removeItemFromCart={props.removeItemFromCart} cartItems={props.cartItems} />
+        {(props.cartItems.length>0 ? <Cartproducts removeItemFromCart={props.removeItemFromCart} cartItems={props.cartItems} /> : noItems)}
         </div>
     )
 }
