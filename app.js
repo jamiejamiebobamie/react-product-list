0821961469901d540bec4eab50ad1d2198e3849c
jamiejamiebobamie <jmccrory@vt.edu>
2019-08-30 23:44:14 -0700

    attempting to push to heroku

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'imgs','cart_icon.png'));
 });
 
 app.listen(9000);
